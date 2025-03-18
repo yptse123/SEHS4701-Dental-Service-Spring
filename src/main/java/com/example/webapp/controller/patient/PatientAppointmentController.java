755960ca098967011ebeca9e76485ceaf71a5540
yptse123 <yptse123@gmail.com>
2025-03-18 11:34:48 +0800

    fix

--- src/main/java/com/example/webapp/controller/patient/PatientAppointmentController.java
+++ src/main/java/com/example/webapp/controller/patient/PatientAppointmentController.java
@@ -7,11 +7,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
